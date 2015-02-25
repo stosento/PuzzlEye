22bded4702e9939901f1848363c5dd9cccc771d2
mpollin77 <mpollin77@gmail.com>
2015-02-24 19:15:39 -0500

    Difficulty Screen Updates

---
+++ PuzzlEye/Assets/ButtonClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClick : MonoBehaviour {
+	private int counter = 0;
+	
+	void OnClick ()
+	{
+		counter++;
+		Debug.Log ("Clicked " + counter.ToString () + " times.");
+	}
+
+	public void Michael() {
+		Debug.Log ("Button Click");
+	}
+}

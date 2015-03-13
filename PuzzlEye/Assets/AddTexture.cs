8bb5329657eb8323b63d571ae7c96fee0dbda5c6
Caleb Alexander <calexan@umich.edu>
2015-03-13 14:17:12 -0400

    Added script for dynamically uploading images

---
+++ PuzzlEye/Assets/AddTexture.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddTexture : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		var url = "file://C:\\Users\\User\\Pictures\\sunset.jpg";
+		var www = new WWW(url);
+
+		yield return www;
+
+		var ThisTexture = new Texture2D (4, 4, TextureFormat.DXT1, false);
+
+		www.LoadImageIntoTexture(ThisTexture);
+
+		renderer.material.mainTexture = ThisTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

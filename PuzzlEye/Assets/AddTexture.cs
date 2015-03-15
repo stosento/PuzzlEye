4d00c877c4d023cfb7f7d41dc70b42a01027310b
davidzick <zickd@umich.edu>
2015-03-15 13:49:47 -0400

    working on splitting plane into smaller jigsaw pieces

--- PuzzlEye/Assets/AddTexture.cs
+++ PuzzlEye/Assets/AddTexture.cs
@@ -4,21 +4,50 @@ using System.Collections;
 public class AddTexture : MonoBehaviour {
 
 	// Use this for initialization
-	IEnumerator Start () {
-		var url = "file://C:\\Users\\User\\Pictures\\sunset.jpg";
+	public static IEnumerator ApplyTexture (Texture finalTexture, 
+	                                        PuzzleLocations puzzleLocation) 
+	{
+		var url = "file://C:\\Users\\David\\Pictures\\david1.jpg";
 		var www = new WWW(url);
 
+		// Wait for picture to fully load
 		yield return www;
+		
+		var entireTexture = new Texture2D (4, 4, TextureFormat.DXT1, false);
 
-		var ThisTexture = new Texture2D (4, 4, TextureFormat.DXT1, false);
+		www.LoadImageIntoTexture(entireTexture);
 
-		www.LoadImageIntoTexture(ThisTexture);
+//
+//		int height = entireTexture.height / 2;
+//		int width = entireTexture.width / 2;
+//
+//		int startWidth = 0;
+//		int startHeight = 0;
+//
+//		if (puzzleLocation == PuzzleLocations.TopLeft)
+//			startHeight = height;
+//		else if (puzzleLocation == PuzzleLocations.TopRight) {
+//			startHeight = height;
+//			startWidth = width;
+//		} else if (puzzleLocation == PuzzleLocations.BottomRight)
+//			startWidth = width;
+//
+//		
+//		// Get the pixel block and place it into a new texture.
+//		var pix = entireTexture.GetPixels(startWidth, startHeight, width, height);
+//		var destTex = new Texture2D(width, height);
+//		destTex.SetPixels(pix);
+//		destTex.Apply();
 
-		renderer.material.mainTexture = ThisTexture;
+		//finalTexture = destTex;
+		finalTexture = entireTexture;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public enum PuzzleLocations
+	{
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight
 	}
 }

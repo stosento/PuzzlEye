d680a0e9b46e1dc9bc7842c1b8f45efb086168a3
Caleb Alexander <calexan@umich.edu>
2015-03-15 14:57:11 -0400

    Image now transferred between scenes

--- PuzzlEye/Assets/UploadImage.cs
+++ PuzzlEye/Assets/UploadImage.cs
@@ -7,7 +7,7 @@ public class UploadImage : MonoBehaviour {
 	public static string FinalPath;
 
 	public void ImageDialog() {
-		var path = EditorUtility.OpenFilePanel ("Select Image", "C:\\", "png");
+		var path = EditorUtility.OpenFilePanel ("Select Image", "C:\\", "png;*.jpg;*.jpeg");
 
 		if (path != null) {
 			FinalPath = path;

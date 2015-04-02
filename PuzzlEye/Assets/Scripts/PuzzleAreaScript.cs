a18b2de8cdd0604ab968111673ea1e004c4ca3e8
Caleb Alexander <calexan@umich.edu>
2015-04-02 10:05:10 -0400

    Randomization never ends in template area

--- PuzzlEye/Assets/Scripts/PuzzleAreaScript.cs
+++ PuzzlEye/Assets/Scripts/PuzzleAreaScript.cs
@@ -28,8 +28,12 @@ public class PuzzleAreaScript : MonoBehaviour {
 	void GeneratePiecePlanes(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
+
 		
 		piecePlanes = new GameObject[rows][];
+		
+		float rowInverse = 1.0f / rows;
+		float columnInverse = 1.0f / columns;
 
 		for (int i = 0; i < rows; i++) {
 			piecePlanes[i] = new GameObject[columns];
@@ -37,9 +41,15 @@ public class PuzzleAreaScript : MonoBehaviour {
 			for (int j = 0; j < columns; j++) {
 				piecePlanes[i][j] = GameObject.CreatePrimitive(PrimitiveType.Plane);
 				piecePlanes[i][j].transform.parent = GameObject.Find("PuzzleArea").transform;
+
+				float randX = 0f;
+				float randY = 0f;
+
+				while (Mathf.Abs(randX) <= 5f) randX = Random.Range (-20f, 20f);
+				while (Mathf.Abs(randY) <= 5f) randY = Random.Range (-7f, 7f);
 				
-				piecePlanes[i][j].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-				piecePlanes[i][j].transform.localPosition = new Vector3( Random.Range(-5f,5f),Random.Range (-2f,2f), 0f);
+				piecePlanes[i][j].transform.localScale = new Vector3(rowInverse*1f, columnInverse*1f, columnInverse*1f);
+				piecePlanes[i][j].transform.localPosition = new Vector3( randX,2f, randY);
 				piecePlanes[i][j].transform.Rotate(new Vector3(90f, 180f, 0f));
 				piecePlanes[i][j].renderer.material.mainTexture = AddDynamicTexture.ApplyDynamicTexture(i, j, DifficultySelectionScript.Difficulty);
 			}

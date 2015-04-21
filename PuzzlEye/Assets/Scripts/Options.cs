985060781388686c2916d7fbf3f3d60daffebc09
Stephen <stosento@umich.edu>
2015-04-20 21:03:21 -0400

    Make Menu Text Larger

--- PuzzlEye/Assets/Scripts/Options.cs
+++ PuzzlEye/Assets/Scripts/Options.cs
@@ -6,32 +6,44 @@ public class Options: MonoBehaviour {
 	private string	msg;
 	private bool 	showbuttons = false;
 	private bool	readytoquit = false;
+	private GUIStyle largetext;
 
 	void Awake() {
 		Application.targetFrameRate=30;
 	}
 
+	void Start() {
+		largetext = new GUIStyle("button");
+		largetext.fontSize = 17;
+		largetext.normal.textColor = Color.white;
+	}
+
 	void OnGUI() {
-		if (GUI.Button (new Rect (10,10,120,30), showbuttons?"Hide Menu":"Show Menu")) {
+		if (GUI.Button (new Rect (10,10,130,100), showbuttons?"Hide Menu":"Show Menu")) {
 			showbuttons=!showbuttons;
 		}
 		if (showbuttons) {
-			if (GUI.Button (new Rect (10,40,120,30), visible?"Hide Fingertips":"Show Fingertips")) {
+			GUI.color = Color.magenta;
+			if (GUI.Button (new Rect (10,110,130,100), visible?"Hide Fingertips":"Show Fingertips", largetext)) {
 				visible=!visible;
 			}
-			if (GUI.Button (new Rect (10,70,120,30), "Restart")) {
+			GUI.color = Color.green;
+			if (GUI.Button (new Rect (10,210,130,100), "Restart", largetext)) {
 				Application.LoadLevel(Application.loadedLevel);
 			}
-			if (GUI.Button (new Rect (10,100,120,30), "Main Menu")) {
+			GUI.color = Color.grey;
+			if (GUI.Button (new Rect (10,310,130,100), "Main Menu", largetext)) {
 				Application.LoadLevel(0);
 			}
-			if (GUI.Button (new Rect (10,130,130,30), readytoquit?"Press Again to Quit":"Quit")) {
+			GUI.color = Color.red;
+			if (GUI.Button (new Rect (10,410,130,100), readytoquit?"Press Again to Quit":"Quit", largetext)) {
 				if (readytoquit)
 					Application.Quit();
 				else
 					readytoquit=!readytoquit;
 			}
-			if (GUI.Button (new Rect (10, 160, 130, 30), "Choose New Puzzle")) {
+			GUI.color = Color.cyan;
+			if (GUI.Button (new Rect (10, 510, 130, 100), "New Puzzle", largetext)) {
 				Application.LoadLevel(2);		
 			}
 		}
